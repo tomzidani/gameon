6c4bb0c52918878d6435de0a9c1e4754f915a128
Tom Zidani <tom.zidani@gmail.com>
2021-11-26 09:36:21 +0100

    Close #1 – Close the modal.
    Functionality added to the close button (x).

--- modal.js
+++ modal.js
@@ -1,23 +1,32 @@
+// Nav
+// ---
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav")
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
+// Modal
+// -----
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const formData = document.querySelectorAll(".formData")
+const modalCloseBtn = document.querySelector(".bground span.close")
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
+// Launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"
 }
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none"
+}
 
+// Modal events
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+modalCloseBtn.addEventListener("click", closeModal)
